322dc8e7a0e5f8b795b413830edafd502fe8010f
rpeace770 <rpeace770@gmail.com>
2017-05-11 18:56:36 -0500

    add array

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,5 +1,5 @@
 puts "What is your name?"
-name = gets.chomp
+user_input = gets.chomp
 
 puts "How old are you?"
 age = gets.chomp.to_i
@@ -14,44 +14,45 @@ puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
 
 # need an array with all the answers?
-# interview = []
-# interview.push(name, age, year, garlic, insurance)
+interview = []
+interview.push(user_input, age, year, garlic, insurance)
+puts interview
 
-wolves_like_sunshine = true
-wolves_like_garlic = true
-vampires_like_sunshine = false
-vampires_like_garlic = false
+# wolves_like_sunshine = true
+# wolves_like_garlic = true
+# vampires_like_sunshine = false
+# vampires_like_garlic = false
 
 # (ruby keyword for current year?)
 
-puts "How many employees will be processed?"
-process_num = gets.chomp.to_i
-count = 0
+# puts "How many employees will be processed?"
+# process_num = gets.chomp.to_i
+# count = 0
 
 # loop for all employees
 # while count < process_num
 
-case interview
+# case interview
 
-when (year - current_year == age) && (garlic == true || insurance == true)
-	status = "Probably not a vampire."
+# when (year - current_year == age) && (garlic == true || insurance == true)
+# 	status = "Probably not a vampire."
 
-when (year - current_year != age) && (garlic == false || insurance == false)
-	status = "Probably a vampire."
+# when (year - current_year != age) && (garlic == false || insurance == false)
+# 	status = "Probably a vampire."
 
-when (year - current_year != age) && (garlic == false && insurance == false)
-	status = "Almost certainly a vampire."
+# when (year - current_year != age) && (garlic == false && insurance == false)
+# 	status = "Almost certainly a vampire."
 
-when (name == "Drake Cula") || (name = "Tu Fang")
-	status = "Definitely a vampire."
+# when (name == "Drake Cula") || (name = "Tu Fang")
+# 	status = "Definitely a vampire."
 
-#else statement????
-when 
-	status = "Results inconclusive"
+# #else statement????
+# when 
+# 	status = "Results inconclusive"
 
-end
+# end
 
-puts status
+# puts status
 
 # end
 

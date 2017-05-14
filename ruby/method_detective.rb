892d40b60bc016037e98cf3603ed999984deaf1e
rpeace770 <rpeace770@gmail.com>
2017-05-13 22:28:42 -0500

    add methods to strings

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,44 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(2, "o")
+"zom".replace("zoom")
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".insert(9, " suspects")
+"the usual" << " suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
+" suspects".insert(0, "the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".delete("T")
+"The mystery of the missing first letter".slice(1, 38)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".delete("  ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".bytesize
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# The letter z has a label of 122 in the American Standard Code for Information Interchange (ASCII)
+# 122 represents the z characted in the ASCII code.
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
+
+

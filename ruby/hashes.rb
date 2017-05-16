2116ea59b8dab8725f3e426a22700349a45fd63c
rpeace770 <rpeace770@gmail.com>
2017-05-15 19:06:16 -0500

    add psuedocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,18 @@
+# clients name, age, number of children, decor theme, etc
+# need strings, integers, and boolean data
+# keys should be symbols UNLESS a string is needed
+
+#prompt user for all information
+
+#convert user input to appropriate data type
+
+#print hash to screen when all questions are answered
+
+#give user opportunity to update a key, no loop, once is fine
+#if designer says none, skip the update
+
+#use string method to turn into a symbol
+
+#no need for user errors
+
+#print latest hash and exit

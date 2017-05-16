611b6629ac1a0fff18fe04fe45c2d89b10c65265
rpeace770 <rpeace770@gmail.com>
2017-05-15 19:19:46 -0500

    add driver code for application

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -16,3 +16,56 @@
 #no need for user errors
 
 #print latest hash and exit
+
+#METHODS
+
+client = {
+	name: "",
+	age: "",
+	num_children: "",
+	decor_theme: "",
+	bedrooms: "",
+	pets: "",
+	laundry: ""
+}
+
+#DRIVER CODE
+
+puts "What is your full name?"
+client[:name] = gets.chomp
+
+puts "What is your age?"
+client[:age] = gets.chomp.to_i
+
+puts "How many children do you have?"
+client[:num_children] = gets.chomp.to_i
+
+puts "What is your preferred decor theme?"
+client[:decor_theme] = gets.chomp
+
+puts "How many bedrooms do you need?"
+client[:bedrooms] = gets.chomp.to_i
+
+puts "Do you have a pet(s)?"
+client[:pets] = gets.chomp
+
+puts "Do you need a laundry room?"
+client[:laundry] = gets.chomp
+
+p client[:name]
+p client[:age]
+p client[:num_children]
+p client[:decor_theme]
+p client[:bedrooms]
+p client[:pets]
+p client[:laundry]
+
+
+
+
+
+
+
+
+
+

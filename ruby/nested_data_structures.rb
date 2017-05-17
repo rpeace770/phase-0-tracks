cb2fdd5f6d649d79eee27c1c31aeabb472970324
rpeace770 <rpeace770@gmail.com>
2017-05-16 22:54:09 -0500

    add statements using data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,17 +1,26 @@
 office = {
 	kitchen: [
 		"microwave", "fridge", "sink", "stove"
-	]
+	],
 	studio: {
 		people: 4,
 		dogs: "Penny"
-	}
+	},
 	main: {
 		people: 17,
 		dogs: [
-			"Kosar", "Jimmy", "Vera", "Rufus", "Ranger", "Molly"
+			"Kosar",
+			"Jimmy",
+			"Vera",
+			"Rufus",
+			"Ranger",
+			"Molly"
 		]
 	}
 }
 
-puts [:office][:main]
+puts office[:main][:dogs].sort
+
+puts office[:studio][:people] + office[:main][:people]
+
+puts office[:main][:dogs][3]

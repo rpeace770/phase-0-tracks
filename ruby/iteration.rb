e1d5021e129b6ca5c65c2a7a512228b6eaef38a6
rpeace770 <rpeace770@gmail.com>
2017-05-16 19:56:41 -0500

    add hash and array

---
+++ ruby/iteration.rb
@@ -0,0 +1,19 @@
+# def make_red
+#     puts "Red is the only color for cars"
+#     yield("corvette","lamburgini")
+#     puts "All the cars red!"
+# end
+
+# make_red { |car1, car2| puts "#{car1} is now red! #{car2} is also red!"}
+
+#declare array and hash
+
+fruit = ["apple", "banana", "pear", "grape", "orange"]
+
+best_pie = {
+	apple: "american"
+	pumpkin: "best"
+	cherry: "gross"
+	buttermilk: "sweet"
+}
+

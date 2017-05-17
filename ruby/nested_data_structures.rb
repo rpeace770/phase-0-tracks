96794a70f9a8ccb7a1ccc0f4face69406476e1ff
rpeace770 <rpeace770@gmail.com>
2017-05-16 22:40:01 -0500

    built office nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,17 @@
+office = {
+	kitchen: [
+		"microwave", "fridge", "sink", "stove"
+	]
+	studio: {
+		people: 4,
+		dogs: "Penny"
+	}
+	main: {
+		people: 17,
+		dogs: [
+			"Kosar", "Jimmy", "Vera", "Rufus", "Ranger", "Molly"
+		]
+	}
+}
+
+puts [:office][:main]

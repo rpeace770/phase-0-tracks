313327161474451b8abb0ff0d4bf83a9717c12e8
rpeace770 <rpeace770@gmail.com>
2017-05-17 22:22:04 -0500

    loop for vowel and consonant change

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,10 +8,10 @@
 #reverse name
 
 puts "What is your first name?"
-first_name = gets.chomp
+first_name = gets.chomp.downcase
 
 puts "What is your last name?"
-last_name = gets.chomp
+last_name = gets.chomp.downcase
 
 full_name = [first_name, last_name]
 reverse_name = full_name.reverse!.join(" ")
@@ -19,14 +19,22 @@ puts reverse_name
 
 # next vowel
 
-vowels = ["a", "e", "i", "o", "u"]
+vowels = "aeiou"
+consonants = "bcdfghjklmnpqrstvwxyz"
 index = 0
+alias_name = ""
 
 while index < reverse_name.length
-		new_vowel_index = vowels.index(user_vowel) + 1
-		new_vowel = vowels[new_vowel_index]
+		if reverse_name[index].include?("a")
+			new_vowel_index = vowels.index(reverse_name[index]) + 1
+			alias_name[index] = vowels[new_vowel_index]
+		else
+			new_consonant_index = consonants.index(reverse_name[index]) + 1
+			alias_name[index] = consonants[new_consonant_index]
+		end
+		index += 1
 end
-puts new_vowel
+puts alias_name
 
 # #put in e
 # #add one to index

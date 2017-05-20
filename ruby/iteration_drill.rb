3e5ff17758562418b1c05319134dc9dd431b0f5a
rpeace770 <rpeace770@gmail.com>
2017-05-19 22:10:32 -0500

    finish first iteration drill

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -5,10 +5,14 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
+
+zombie_apocalypse_supplies.each { |item| puts item + "*"}
 # ----
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
+
+
 # ----
 
 # 3. Create a method to see if a particular item (string) is in the

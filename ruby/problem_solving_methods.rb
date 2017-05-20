08552a936d5959327c235fe540cd72a76ba0e4b5
rpeace770 <rpeace770@gmail.com>
2017-05-20 11:35:56 -0500

    add fibonacci method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -23,3 +23,39 @@ puts search_array(array, 25)
 
 # ran this line to make sure the value was nil
 # puts search_array(array, 25).nil?
+
+#FIBONACCI SEQUENCE
+
+def fibonacci(num)
+  array = [0,1]
+
+  if num == 0
+    return []
+  elsif num == 1
+    return [0]
+  end
+
+
+  index_one = 0
+  index_two = 1
+
+  while array.length < num
+    previous_one = array[index_one]
+    previous_two = array[index_two]
+
+    array.push(previous_one + previous_two)
+
+    index_one += 1
+    index_two += 1
+  end
+
+  array
+end
+
+## Automating fib(100) giant number test
+# puts fibonacci(100).last == 218922995834555169026
+
+## TESTING
+# p fibonacci(0)
+# p fibonacci(1)
+# p fibonacci(6)

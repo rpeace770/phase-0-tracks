ef0865033a6322cfcbb3c114f1cb35152c21130b
rpeace770 <rpeace770@gmail.com>
2017-05-22 20:17:26 -0500

    add methods to Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,38 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+		puts "Woof" * integer
+	end
+
+	def roll_over()
+		puts "Roll over"
+	end
+
+	def dog_years(human_years)
+		#convert human years to dog years
+		p human_years / 7.0
+	end
+
+	def shake()
+		puts "Shake"
+	end
+
 end
 
+#DRIVER CODE
+
+#intialize instance of Puppy
+#fetch a ball
+
+Rufus = Puppy.new
+
+Rufus.fetch("ball")
+
+Rufus.speak(3)
+
+Rufus.roll_over
+
+Rufus.dog_years(30)
+
+Rufus.shake

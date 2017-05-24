5aeab88ed850d6191f57dc3d7eb115adeda046f4
rpeace770 <rpeace770@gmail.com>
2017-05-23 21:13:46 -0500

    assign random number to age

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,7 +2,7 @@ class Santa
 	#getter methods
 	attr_reader :age, :ethnicity
 	#setter methods or just use accessor if you want to use same variable for setter and getter
-	attr_accessor :gender
+	attr_accessor :gender, :age
 	#attr_writer - only writing, not readable
 
 	def speak
@@ -26,29 +26,39 @@ class Santa
 	end
 
 	def get_mad_at(reindeer_name)
+		#get index of reindeer name
 		switch_index = @reindeer_ranking.index(reindeer_name)
+		#delete the reindeer from the array
 		@reindeer_ranking.delete_at(switch_index)
+		#reassign reindeer to the end
 		@reindeer_ranking << reindeer_name
 		p @reindeer_ranking
 		end
 
 end
 
+#empty array for new santas
 santas = []
-
-genders = ["agender", "female", "bigender", "male", "gender fluid", "N/A", "transgender"]
+#arrays for random choosing
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 ethnicities = ["Swedish", "black", "Iranian", "Korean-American", "white", "Norwegian", "N/A"]
 
 i = 0
 while i < 100
 	santas << Santa.new(genders.sample, ethnicities.sample)
-	puts santas[i].age
-	puts santas[i].ethnicity
-	puts santas[i].gender
+	#choose random age for the new Santa
+	santas[i].age = rand(140)
+	#set to variables
+	santa_age = santas[i].age
+	santa_ethnicity = santas[i].ethnicity
+	santa_gender = santas[i].gender
+	#print out attributes
+	puts "Age: #{santa_age}"
+	puts "Ethnicity: #{santa_ethnicity}"
+	puts "Gender: #{santa_gender}"
+	#add a count to the loop
 	i += 1
 end
-
-
 
 #TEST CODE
 
@@ -58,7 +68,6 @@ end
 # Nick.get_mad_at("Comet")
 # Nick.gender = "female"
 # p Nick
-
 # p Nick.age
 # p Nick.ethnicity
 

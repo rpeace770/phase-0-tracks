dc11385d033050e8e37d304ef9a7782c175575ca
rpeace770 <rpeace770@gmail.com>
2017-05-31 23:22:22 -0500

    psuedocode for shared values method

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,5 @@
+//METHODS
+
 // write function that returns longest word or phrase in an array
 // .length gives the length of a string
 // make sample array
@@ -19,10 +21,37 @@ function longestWord(array) {
 
 //write a function that takes two objects and checks to see if the objects share at least one key-value pair.
 
+//yourFunction({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+//evaluates to true
+
+function share(object, object2) {
+	//check object key/value1 with object2 key/value1
+	//check object key/value1 with object2 key/value2
+	//check object key/value2 with object2 key/value1
+	//check object key/value2 with object2 key/value2
+	// if (object[key] == object2[key]) {
+	// 	true
+	// }
+	// else if (object[key] == object2[value]) {
+	// 	true
+	// }
+	// else if (object[value] == object2[key]) {
+	// 	true
+	// }
+	// else if (object[value] == object2[value]) {
+	// 	true
+	// }
+	// else {
+	// 	false
+	// }
+	console.log(Object.values(object));
+};
+
+
+//DRIVER CODE
+
 var words = ["cat", "paint", "happiness", "no", "miraculous"];
 longestWord(words);
 
-
-
-
+share({name: "Steven", age: 54}, {name: "Tamir", age: 54});
 

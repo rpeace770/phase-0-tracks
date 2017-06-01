12c49971ec79d0b11804f8c6da93b092556966af
rpeace770 <rpeace770@gmail.com>
2017-06-01 10:14:11 -0500

    add method to test random words

--- js/algos.js
+++ js/algos.js
@@ -16,7 +16,7 @@ function longestWord(array) {
 			longest = array[i];
 		}
 	}
-	console.log(longest)
+	return longest
 };
 
 //write a function that takes two objects and checks to see if the objects share at least one key-value pair.
@@ -36,29 +36,44 @@ function key_share(object, object2) {
 	//check object key/value2 with object2 key/value2
 
 	if (keys[0] === keys2[0] && values[0] === values2[0]) {
-		true
+		return true
 	} 
 	else if (keys[0] === keys2[1] && values[0] === values2[1]) {
-		true
+		return true
 	}
 	else if (keys[1] === keys2[0] && values[1] === values2[0]) {
-		true
+		return true
 	}
 	else if (keys[1] === keys2[1] && values[1] === values2[1]) {
-		true
+		return true
 	}
 	else {
-		false
+		return false
 	}
 	
 };
 
 
 function test_data(integer) {
-	//build array of strings according to integer
-	//min 1 letter and max 10 letter
-	//generate random length for each word
-	// Math.floor((Math.random() * 10) + 1);
+	//loop to make integer number of words
+	word_array = []
+	for(i = 0; i < integer; i++) {
+		var alphabet = "abcdefghijklmnopqrstuvwxyz"
+		//build array of strings according to integer
+		//min 1 letter and max 10 letter
+		//generate random length for each word
+		var word = []
+		//Math.random selects a random number between 0 and 1
+		//multiply the decimal by the highest number in the chosen range and add one to account for 0
+		//math.floor rounds down to the nearest whole number
+		//loop for creating words 1-10 letters long with random letters
+		for(j = 0; j < Math.floor((Math.random() * 10) + 1); j++) {
+			word.push(alphabet.charAt(Math.floor(Math.random() * 26)));
+			var random_word = word.join("");
+		}
+		word_array.push(random_word);
+	}
+	return word_array;
 }
 
 //DRIVER CODE
@@ -67,4 +82,15 @@ var words = ["cat", "paint", "happiness", "no", "miraculous"];
 longestWord(words);
 
 key_share({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+
+for(i = 0; i < 10; i++) {
+	var longest_array = test_data(3);
+	console.log(longest_array);
+	var result = longestWord(longest_array);
+	console.log(result);
+};
+
+//LOOP NOT WORKING, FIX BEFORE TURN IN
+
+
 

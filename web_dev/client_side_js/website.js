d608c926877a495d4da3c6e4f98c7e08baad5905
rpeace770 <rpeace770@gmail.com>
2017-06-13 13:50:06 -0500

    Add event listener to website

--- web_dev/client_side_js/website.js
+++ web_dev/client_side_js/website.js
@@ -1,2 +1,10 @@
-var one = document.getElementsById('one');
+var one = document.getElementById("one");
 one.style.border = "10px solid blue";
+
+function textAlign(event) {
+	console.log(event);
+	event.target.style.textAlign = "center";
+}
+
+var text = document.getElementById("two");
+text.addEventListener("click", textAlign);

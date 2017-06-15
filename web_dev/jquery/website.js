4e24b09210f3f22151d49da2b0edd93f66a512d7
rpeace770 <rpeace770@gmail.com>
2017-06-15 08:57:06 -0500

    Comment out code

--- web_dev/jquery/website.js
+++ web_dev/jquery/website.js
@@ -1,13 +1,13 @@
-var one = document.getElementById("header");
-one.style.border = "10px solid blue";
+// var one = document.getElementById("header");
+// one.style.border = "10px solid blue";
 
-var borders = document.querySelectorAll('p.one');
-for (var i = 0; i < borders.length; i++) {
-	borders[i].style.textAlign = "center";
-}
+// var borders = document.querySelectorAll('p.one');
+// for (var i = 0; i < borders.length; i++) {
+// 	borders[i].style.textAlign = "center";
+// }
 
-function highlight(event) {
-	event.target.style.borderColor = "red";
-}
+// function highlight(event) {
+// 	event.target.style.borderColor = "red";
+// }
 
-borders[1].addEventListener("mouseover", highlight);
+// borders[1].addEventListener("mouseover", highlight);

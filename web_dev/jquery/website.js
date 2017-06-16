7048b7b780f6cffd849e0dc55d62e98321319670
rpeace770 <rpeace770@gmail.com>
2017-06-16 12:07:10 -0500

    Add function to hide header

--- web_dev/jquery/website.js
+++ web_dev/jquery/website.js
@@ -1,5 +1,5 @@
 // insert paragraph and delay the display of the first paragraph
-$('p:first').before('<p>Inserting here</p>').hide().delay(500).fadeIn(1400);
+$('p:first').before('<p>Inserting here.</p>').hide().delay(500).fadeIn(1400);
 
 //function to add words to each paragraph.
 $(function() {
@@ -9,3 +9,21 @@ $(function() {
 	});
 });
 
+//function to remove paragraph when its clicked on
+$(function() {
+	$('p').on("click", function() {
+		$(this).animate({
+			paddingLeft: '+=780'
+		}, 1200, function(){
+			$(this).fadeOut(500);
+			// $(this).remove();
+		});
+	});
+});
+
+//function that hides the header when clicked on
+$(function() {
+	$('h1').on("click", function() {
+		$(this).slideUp();
+	});
+});

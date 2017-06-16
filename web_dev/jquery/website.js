85d50a21b7ffe01b8bb7fc90ad736bbf834673b4
rpeace770 <rpeace770@gmail.com>
2017-06-16 10:02:19 -0500

    Remove old javascript

--- web_dev/jquery/website.js
+++ web_dev/jquery/website.js
@@ -1,15 +1,1 @@
-// var one = document.getElementById("header");
-// one.style.border = "10px solid blue";
-
-// var borders = document.querySelectorAll('p.one');
-// for (var i = 0; i < borders.length; i++) {
-// 	borders[i].style.textAlign = "center";
-// }
-
-// function highlight(event) {
-// 	event.target.style.borderColor = "red";
-// }
-
-// borders[1].addEventListener("mouseover", highlight);
-
-	$('p:first').before('<p>Inserting here</p>');
+$('p:first').before('<p>Inserting here</p>');
